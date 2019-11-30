da10cd6f2add0ffaf9b45ef511488330366a1746
mtarunpr <m.tarunprasad@gmail.com>
2019-11-30 15:39:45 -0500

    Added difficulty levels

--- assignment_distro/main.lua
+++ assignment_distro/main.lua
@@ -5,6 +5,8 @@ function love.load()
     bestScore = 0;
     color = 1;
 
+    gameStarted = false;
+
     gridXCount = 40;
     gridYCount = 25;
 
@@ -67,6 +69,8 @@ function love.load()
         end
         
         score = 0;
+        stage = 1;
+        color = 1;
 
         moveFood();       
     end
@@ -75,93 +79,95 @@ function love.load()
 end
 
 function love.update(dt)
-    timer = timer + dt;
-    
-    gameTimer = gameTimer + dt;
+    if gameStarted then
+        timer = timer + dt;
+        
+        gameTimer = gameTimer + dt;
 
-    if snakeAlive then
-         if (gameTimer >= 1) then
-           gui.timer = gui.timer + 1;
-           gameTimer = 0;
-           end
-         
-      
-        local timerLimit = 0.06; -- snake speed
-        if timer >= timerLimit then
-            timer = timer - timerLimit;
-
-            if #directionQueue > 1 then
-                table.remove(directionQueue, 1);
+        if snakeAlive then
+            if (gameTimer >= 1) then
+            gui.timer = gui.timer + 1;
+            gameTimer = 0;
             end
+            
+        
+            local timerLimit = 0.09 - 0.02 * level; -- snake speed
+            if timer >= timerLimit then
+                timer = timer - timerLimit;
 
-            local nextXPosition = snakeSegments[1].x;
-            local nextYPosition = snakeSegments[1].y;
-
-            if directionQueue[1] == 'right' then
-                nextXPosition = nextXPosition + 1;
-                if nextXPosition > gridXCount then
-                    --table.remove(snakeSegments);
-                    snakeAlive = false;
+                if #directionQueue > 1 then
+                    table.remove(directionQueue, 1);
                 end
-                
-            elseif directionQueue[1] == 'left' then
-                nextXPosition = nextXPosition - 1;
-                if nextXPosition < 1 then
-                    --table.remove(snakeSegments);
-                    snakeAlive = false;
-                end
-                
-                -- all this stuff is collision
-                
-            elseif directionQueue[1] == 'down' then
-                nextYPosition = nextYPosition + 1;
-                if nextYPosition > (gridYCount + 3) then
-                    --table.remove(snakeSegments);
-                    snakeAlive = false;
-                end
-                
-            elseif directionQueue[1] == 'up' then
-                nextYPosition = nextYPosition - 1;
-                if nextYPosition < 4 then
-                    --table.remove(snakeSegments);
-                    snakeAlive = false;
-                end
-            end
 
-            local canMove = true;
+                local nextXPosition = snakeSegments[1].x;
+                local nextYPosition = snakeSegments[1].y;
 
-            for segmentIndex, segment in ipairs(snakeSegments) do
-                if segmentIndex ~= #snakeSegments
-                and nextXPosition == segment.x 
-                and nextYPosition == segment.y then -- this stops the player going back on itself
-                    canMove = false;
-                end
-            end
-
-            if canMove then
-                table.insert(snakeSegments, 1, {x = nextXPosition, y = nextYPosition})
-
-                if snakeSegments[1].x == foodPosition.x
-                and snakeSegments[1].y == foodPosition.y then
-                    moveFood();
-                    score = score + color;
-                    if score <= 10 then
-                        stage = 1
-                    elseif score <= 20 then
-                        stage = 2
-                    elseif score <= 40 then
-                        stage = 3
+                if directionQueue[1] == 'right' then
+                    nextXPosition = nextXPosition + 1;
+                    if nextXPosition > gridXCount then
+                        --table.remove(snakeSegments);
+                        snakeAlive = false;
                     end
-                    color = math.random(stage);
-                else
-                    table.remove(snakeSegments);
+                    
+                elseif directionQueue[1] == 'left' then
+                    nextXPosition = nextXPosition - 1;
+                    if nextXPosition < 1 then
+                        --table.remove(snakeSegments);
+                        snakeAlive = false;
+                    end
+                    
+                    -- all this stuff is collision
+                    
+                elseif directionQueue[1] == 'down' then
+                    nextYPosition = nextYPosition + 1;
+                    if nextYPosition > (gridYCount + 3) then
+                        --table.remove(snakeSegments);
+                        snakeAlive = false;
+                    end
+                    
+                elseif directionQueue[1] == 'up' then
+                    nextYPosition = nextYPosition - 1;
+                    if nextYPosition < 4 then
+                        --table.remove(snakeSegments);
+                        snakeAlive = false;
+                    end
                 end
-            else
-                snakeAlive = false;
+
+                local canMove = true;
+
+                for segmentIndex, segment in ipairs(snakeSegments) do
+                    if segmentIndex ~= #snakeSegments
+                    and nextXPosition == segment.x 
+                    and nextYPosition == segment.y then -- this stops the player going back on itself
+                        canMove = false;
+                    end
+                end
+
+                if canMove then
+                    table.insert(snakeSegments, 1, {x = nextXPosition, y = nextYPosition})
+
+                    if snakeSegments[1].x == foodPosition.x
+                    and snakeSegments[1].y == foodPosition.y then
+                        moveFood();
+                        score = score + color;
+                        if score < 10 then
+                            stage = 1
+                        elseif score < 40 then
+                            stage = 2
+                        else
+                            stage = 3
+                        end
+                        color = math.random(stage);
+                    else
+                        table.remove(snakeSegments);
+                    end
+                else
+                    snakeAlive = false;
+                end
             end
+        elseif timer >= 1.5 then
+            reset(); -- when the snake dies, the timer stops. if the timer has stopped for 1.5 seconds, restart the game.
         end
-    elseif timer >= 1.5 then
-        reset(); -- when the snake dies, the timer stops. if the timer has stopped for 1.5 seconds, restart the game.
     end
 end
 
@@ -179,50 +185,75 @@ function love.draw()
         love.graphics.rectangle('fill', (x - 1) * cellSize, (y - 1) * cellSize, cellSize - 1, cellSize - 1); -- draw entity function
     end
 
-    for segmentIndex, segment in ipairs(snakeSegments) do
-        if snakeAlive then
-            love.graphics.setColor(173/255, 255/255, 47/255); -- snake colour
-        else
-            love.graphics.setColor(70/255, 70/255, 70/255); -- dead snake colour
-        end
-        drawCell(segment.x, segment.y); -- draw snake
-    end
-
-    if color == 1 then
-        love.graphics.setColor(1, 0, 0); -- food colour
-    elseif color == 2 then
-        love.graphics.setColor(0, 1, 0);
+    if not gameStarted then
+        love.graphics.setColor(1, 1, 1);
+        font = love.graphics.newFont(15); -- font size+
+        love.graphics.setFont(font);
+        xoffset = 250;
+        yoffset = 175;
+        love.graphics.print("Choose level:", xoffset, yoffset);
+        love.graphics.print("1 - Easy", xoffset, yoffset + 20);
+        love.graphics.print("2 - Medium", xoffset, yoffset + 40);
+        love.graphics.print("3 - Hard", xoffset, yoffset + 60);
     else
-        love.graphics.setColor(0, 0, 1);
+        for segmentIndex, segment in ipairs(snakeSegments) do
+            if snakeAlive then
+                love.graphics.setColor(173/255, 255/255, 47/255); -- snake colour
+            else
+                love.graphics.setColor(70/255, 70/255, 70/255); -- dead snake colour
+            end
+            drawCell(segment.x, segment.y); -- draw snake
+        end
+
+        if color == 1 then
+            love.graphics.setColor(1, 0, 0); -- food colour
+        elseif color == 2 then
+            love.graphics.setColor(0, 1, 0);
+        else
+            love.graphics.setColor(0, 0, 1);
+        end
+
+        drawCell(foodPosition.x, foodPosition.y);
     end
 
-    drawCell(foodPosition.x, foodPosition.y);
-    
     drawGUI();
 end
 
 function love.keypressed(key)
-    if (key == "right" or key == "d")
-    and directionQueue[#directionQueue] ~= 'right'
-    and directionQueue[#directionQueue] ~= 'left' then
-        table.insert(directionQueue, 'right')
+    if not gameStarted then
+        if key == '1' then
+            level = 1
+            gameStarted = true
+        elseif key == '2' then
+            level = 2
+            gameStarted = true
+        elseif key == '3' then
+            level = 3
+            gameStarted = true
+        end
+    else
+        if (key == "right" or key == "d")
+        and directionQueue[#directionQueue] ~= 'right'
+        and directionQueue[#directionQueue] ~= 'left' then
+            table.insert(directionQueue, 'right')
 
-    elseif (key == "left" or key == "a")
-    and directionQueue[#directionQueue] ~= 'left'
-    and directionQueue[#directionQueue] ~= 'right' then
-        table.insert(directionQueue, 'left')
+        elseif (key == "left" or key == "a")
+        and directionQueue[#directionQueue] ~= 'left'
+        and directionQueue[#directionQueue] ~= 'right' then
+            table.insert(directionQueue, 'left')
 
-   -- This is handling key presses.
+    -- This is handling key presses.
 
-    elseif (key == "up" or key == "w")
-    and directionQueue[#directionQueue] ~= 'up'
-    and directionQueue[#directionQueue] ~= 'down' then
-        table.insert(directionQueue, 'up')
+        elseif (key == "up" or key == "w")
+        and directionQueue[#directionQueue] ~= 'up'
+        and directionQueue[#directionQueue] ~= 'down' then
+            table.insert(directionQueue, 'up')
 
-    elseif (key == "down" or key == "s")
-    and directionQueue[#directionQueue] ~= 'down'
-    and directionQueue[#directionQueue] ~= 'up' then
-        table.insert(directionQueue, 'down')
+        elseif (key == "down" or key == "s")
+        and directionQueue[#directionQueue] ~= 'down'
+        and directionQueue[#directionQueue] ~= 'up' then
+            table.insert(directionQueue, 'down')
+        end
     end
 end
 
